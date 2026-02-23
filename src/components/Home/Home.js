5f504bf5d645660562a9c710d99abb2848411d44
Shaban Irshad <shabanirshad408@gmail.com>
2026-02-24 00:39:25 +0530

    added about

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,8 +14,8 @@ const Home=()=>{
                         <h2 className={homeStyle.quote}>hey there!, i'm-</h2>
                         <h1 className={homeStyle.name}>Mohd Shaban </h1>
                         <p><b className={homeStyle.quote}>Full Stack developer</b>. A developer with an interest in Computer Science and <u>web-development</u> tech stacks.</p>
-                        <p>I build applicatins that are fast, responsive and user friendly.</p>
-                        <p>I enjoy turning ideas into real-world full-stack web applications.</p>
+                        <p className={homeStyle.point}>🚀I build applications that are fast, responsive and user friendly.</p>
+                        <p className={homeStyle.point}>🎉I enjoy turning ideas into real-world full-stack web applications.</p>
                         <div>
                             <button>Github</button>
                             <button>LinkedIn</button>
@@ -27,7 +27,7 @@ const Home=()=>{
                  </aside>
                
             </section>
-            <section id='about'>
+            <section id='about' className={homeStyle.about}>
                 <h2 className={homeStyle.heading}>A Bit About Me</h2>
                 <hr className={homeStyle.horizontalLine}></hr>
                 <div className={homeStyle.container}>
@@ -39,7 +39,7 @@ const Home=()=>{
 
                             <p>I'm interested in building something awesome with code and automate tasks with code.</p>
 
-                            <p> When I'm not coding I play games with my friends, watch some show on Netflix, or if the weather's good, play basketball!🏀 </p>
+                            <p> When I'm not coding I watch some show on Netflix, or if the weather's good, like to travel! </p>
                        </div>
                     </div>
                     <div className={homeStyle.rightContainer}>
@@ -48,11 +48,16 @@ const Home=()=>{
                             <img src={shaban} alt='my-image'/>
                         </div>
                          <div className={homeStyle.design}>
-                       <div></div><div></div><div></div><div></div><div></div><div></div><div></div> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> <div> <div> <div></div><div> <div></div><div></div><div></div><div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+                       <div></div><div></div><div></div><div></div><div></div><div></div><div></div> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> <div> <div> <div></div><div> <div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div><div></div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
                     </div>
                     </div>
                 </div>
 
+            </section>
+            <section className={homeStyle.skills}>
+                <h2 className={homeStyle.heading}>What I am good at</h2>
+                <p className={homeStyle.skillQuote}>Technologies I've mastered and use to bring ideas to life</p>
+                <hr className={homeStyle.horizontalLine}></hr>
             </section>
             
         </>
