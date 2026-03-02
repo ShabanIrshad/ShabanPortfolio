da41dfef52b2b7f6e203cc646d188020f0a9bf8b
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-02 11:55:21 +0530

    changed2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,75 @@
-import {Link,Outlet} from 'react-router-dom';
-import navStyle from '../Navbar/Navbar.module.css';
-import { useState } from 'react';
-const Navbar=()=>{
+import { Link, Outlet } from "react-router-dom";
+import navStyle from "./Navbar.module.css";
+import { useState } from "react";
 
-    const [active,setActive]=useState('home');
-    return (
-        <>
-            <div className={navStyle.nav}>
-                <div className={navStyle.leftNavDiv}>
-                    
-                    <div className={navStyle.design}>
-                       <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-                    </div>
-                    <h2 className={navStyle.title}>Shaban Irshad</h2>
-                    
-                   
-                </div>
-                <div className={navStyle.rightNavDiv}>
-                    <ul>
-                        <li><Link to='/'className={active==='home'?navStyle.activeLink:''} onClick={()=>setActive('home')}>Home</Link></li>
-                        <li><Link to='/about' className={active==='about'?navStyle.activeLink:''} onClick={()=>setActive('about')}>About</Link></li>
-                        <li><Link to='/skills' className={active==='skills'?navStyle.activeLink:''} onClick={()=>setActive('skills')}>Skills</Link></li>
-                        <li><Link to='/projects' className={active==='project'?navStyle.activeLink:''} onClick={()=>setActive('project')}>Project</Link></li>
-                        <li><Link to='/contact' className={active==='contact'?navStyle.activeLink:''} onClick={()=>setActive('contact')}>Contact</Link></li>
-                    </ul>
-                </div>
-                 <div className={navStyle.hamburger}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                </div>
-            </div>
-            <Outlet/>
-        </>
-    );
-}
+const Navbar = () => {
+  const [active, setActive] = useState("home");
+
+  return (
+    <>
+      <div className={navStyle.nav}>
+        <h2 className={navStyle.title}>Shaban Irshad</h2>
+
+        <div className={navStyle.rightNavDiv}>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={active === "home" ? navStyle.activeLink : ""}
+                onClick={() => setActive("home")}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={active === "about" ? navStyle.activeLink : ""}
+                onClick={() => setActive("about")}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/skills"
+                className={active === "skills" ? navStyle.activeLink : ""}
+                onClick={() => setActive("skills")}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/projects"
+                className={active === "projects" ? navStyle.activeLink : ""}
+                onClick={() => setActive("projects")}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className={active === "contact" ? navStyle.activeLink : ""}
+                onClick={() => setActive("contact")}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className={navStyle.hamburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <Outlet />
+    </>
+  );
+};
 
 export default Navbar;
