614f243ae7ec7fa013d465636c5bc51061b5b3fc
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-18 13:19:31 +0530

    added project styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,10 +13,11 @@ import git from '../../Assets/gi.png';
 import firebase from '../../Assets/fire.png';
 import postman from '../../Assets/postman.webp';
 import vscode from '../../Assets/vscode.png';
-import pro from '../../Assets/pro.jpg';
+// import pro from '../../Assets/pro.jpg';
 import img1 from '../../Assets/img1.png';
 import img2 from '../../Assets/img2.png';
 import img3 from '../../Assets/img3.png';
+import img4 from '../../Assets/img4.png';
 import link from '../../Assets/link.png';
 
 const Home=()=>{
@@ -276,11 +277,11 @@ const Home=()=>{
                 </div>
                 <div className={homeStyle.projectContainer}>
                     <div className={homeStyle.leftProjectContainer}>
-                       <img src={pro} alt='pro_img'/>
+                       <img src={img4} alt='pro_img'/>
                     </div>
                    <div className={homeStyle.rightProjectContainer}> 
                         <div className={homeStyle.desc}>
-                            <h2>Portfolio Website</h2>
+                            <h2>Pager| Message App</h2>
                             <hr/>
                             <p>A responsive and modern portfolio built with React.js and CSS, featuring smooth animations and elegant UI. Showcases my projects, skills, and experience with a clean and professional design.</p>
                         </div>
@@ -288,12 +289,13 @@ const Home=()=>{
                             <div><img src={html} alt='html'/>HTML5</div>
                             <div><img src={css} alt='html'/>CSS3</div>
                             <div><img src={react} alt='html'/>React.js</div>
+                            <div><img src={firebase} alt='html'/>Firebase</div>
                         </div>
                         <div className={homeStyle.links}>
-                            <a href='https://github.com/ShabanIrshad/ShabanPortfolio/tree/main' target='blank'>
+                            <a href='https://github.com/ShabanIrshad/pager' target='blank'>
                                 <img src={git} alt='link'/>
                             </a>
-                            <a href='https://shaban-portfolio-ochre.vercel.app/' target='blank'>
+                            <a href='https://pager-snowy.vercel.app/' target='blank'>
                                 <img src={link} alt='link'/>
                             </a>
                         </div>
